ba5eaea22040742016886572b507155f29475145
luv005 <luv005@proton.me>
2024-08-25 10:58:14 +0800

    changed footer

--- web/src/app/components/footer.tsx
+++ web/src/app/components/footer.tsx
@@ -4,7 +4,10 @@ export const Footer: FC = () => {
   return (
     <div className="text-center flex flex-col items-center text-xs text-zinc-700 gap-1">
       <div className="text-zinc-400">
-      Vega is an AI-powered search engine.
+      Vega is the brightest star in the northern constellation of Lyra.
+      </div>
+      <div className="text-zinc-400">
+      Vega Search is an AI-powered search engine.
       </div>
     </div>
   );
